4363ba02f8758134d2aa2de62ebf7707b438e9fe
Antonio Moraga <antomoraga7@gmail.com>
2025-03-16 20:14:39 +0000

    modified:   Model.py

--- Model.py
+++ Model.py
@@ -1,47 +1,90 @@
-def Conv(image,kernel,bias):
-    # same thing as V0 just with a extra dimension
-    imD = [len(image[0][0]),len(image[0]),len(image)]# dimensions of the input image
-    keD = [len(kernel[0][0]),len(kernel[0]),len(kernel)]# diemnsions of the kernel
-    outD = [1 + imD[0] - keD[0],1 + imD[1] - keD[1],1 + imD[2] - keD[2]]# dimensions of the output
+import random
 
-    output = []# initialize the output list
+class Conv():
+    def __init__(self,kD):
+        # function to intialize all the paramers of the layers
+        # kD is a variablel with the dimensions of the kernel [x,y,z]
 
-    # loop through all the output values
-    for z in range(outD[2]):
-        plane = []# buffer to store the matrix before adding it to output
-        for y in range(outD[1]):
-            row = []# buffer to store the rows before being appended into plane
-            for x in range(outD[0]):
-                o = bias# calculate the specific output value
+        ## initialize the bias between -1 and 1
+        self.bias = 2*random.random()-1
 
-                # loop though all the values in the convolution
-                for kz in range(keD[2]):
-                    for ky in range(keD[1]):
-                        for kx in range(keD[0]):
-                            o += image[z+kz][y+ky][x+kx]*kernel[kz][ky][kx]# add value to output
+        ## initialize the weights
+        self.k = kD# save the kernel dimensions
+        self.kernel = []# initialize the kernel weights list
 
-                row.append(o)
-            plane.append(row)
-        output.append(plane)
+        # loop though all of the kernel values to initialize them
+        for z in range(self.k[2]):
+            plane = []# buffer for the weights of the kernel
 
-    return output
+            for y in range(self.k[1]):
+                row = []# buffer for the weights of the kernel
 
+                for x in range(self.k[0]):
+                    # generate a random weight -1 to 1
+                    w = 2*random.random()-1
+                    row.append(w)
 
+                plane.append(row)
+            self.kernel.append(plane)
+        
+        ## weight initalization is done.
+
+    def Pass(self,image):
+        # perform a forward pass
+        self.input = image# store the input matrix as well
+
+        # calculate all of the dimensions
+        self.im = [len(image[0][0]),len(image[0]),len(image)]
+        self.outputD = [1+self.im[0]-self.k[0],1+self.im[1]-self.k[1],1+self.im[2]-self.k[2]]
+
+        # initialize output list
+        self.output = []
+
+        # loop though all the output values
+        for z in range(self.outputD[2]):
+            plane = []# buffer for output vals
+            for y in range(self.outputD[1]):
+                row = []# buffer for output vals
+
+                for x in range(self.outputD[0]):
+                    o = self.bias
+
+                    # loop though allthe values in convolution of this value
+                    for kz in range(self.k[2]):
+                        for ky in range(self.k[1]):
+                            for kx in range(self.k[0]):
+                                o += self.input[z+kz][y+ky][x+kx]*self.kernel[kz][ky][kx]
+
+                    row.append(o)
+                plane.append(row)
+            self.output.append(plane)
+
+        # Convolution is done
+
+# define the input image
 image = [[[1,2,3],
          [4,5,6],
          [7,8,9]],
-         [[4,5,6],
-         [1,2,3],
+         [[1,2,3],
+         [4,5,6],
          [7,8,9]]]
 
-kernel = [[[1,2],
-          [3,4]]]
+# initialize the kernel as a 2x2x2 shape and make a forward pass
+conv = Conv([2,2,2])
+conv.Pass(image)
 
-bias = 1
-
-output = Conv(image,kernel,bias)
-
-for plane in output:
+# print the outputs
+print("kernel")
+for plane in conv.kernel:
     print("")
+    for row in plane:
+        print(*row)
+
+print("")
+print("output")
+
+for plane in conv.output:
+    print("")
+
     for row in plane:
         print(*row)
