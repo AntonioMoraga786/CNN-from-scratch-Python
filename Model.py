1df05b5622d4b1c65fd1740059ecd529acab98ff
Antonio Moraga <antomoraga7@gmail.com>
2025-03-18 09:24:24 +0000

    Add ability to deal with multiple neurons to CONV layer

--- Model.py
+++ Model.py
@@ -1,32 +1,39 @@
 import random
 
 class Conv():
-    def __init__(self,kD):
+    def __init__(self,kD,n):
         # function to intialize all the paramers of the layers
         # kD is a variablel with the dimensions of the kernel [x,y,z]
-
-        ## initialize the bias between -1 and 1
-        self.bias = 2*random.random()-1
+        self.n = n# number of neurons
+        
+        self.bias = []
+        ## initialize the bias between -1 and 1 for all neurons
+        for i in range(self.n):
+            self.bias.append(2*random.random()-1)
 
         ## initialize the weights
         self.k = kD# save the kernel dimensions
         self.kernel = []# initialize the kernel weights list
 
-        # loop though all of the kernel values to initialize them
-        for z in range(self.k[2]):
-            plane = []# buffer for the weights of the kernel
+        # loop though every neuron
+        for neuron in range(self.n):
+            k = []# buffer for the whole kernel of a neuron
+            # loop though all of the kernel values to initialize them
+            for z in range(self.k[2]):
+                plane = []# buffer for the weights of the kernel
 
-            for y in range(self.k[1]):
-                row = []# buffer for the weights of the kernel
+                for y in range(self.k[1]):
+                    row = []# buffer for the weights of the kernel
 
-                for x in range(self.k[0]):
-                    # generate a random weight -1 to 1
-                    w = 2*random.random()-1
-                    row.append(w)
+                    for x in range(self.k[0]):
+                        # generate a random weight -1 to 1
+                        w = 2*random.random()-1
+                        row.append(w)
 
-                plane.append(row)
-            self.kernel.append(plane)
-        
+                    plane.append(row)
+                k.append(plane)
+            self.kernel.append(k)
+            
         ## weight initalization is done.
 
     def Pass(self,image):
@@ -41,19 +48,20 @@ class Conv():
         self.output = []
 
         # loop though all the output values
-        for z in range(self.outputD[2]):
+        for n in range(self.n):
             plane = []# buffer for output vals
+
             for y in range(self.outputD[1]):
                 row = []# buffer for output vals
 
                 for x in range(self.outputD[0]):
-                    o = self.bias
+                    o = self.bias[n]
 
                     # loop though allthe values in convolution of this value
                     for kz in range(self.k[2]):
                         for ky in range(self.k[1]):
                             for kx in range(self.k[0]):
-                                o += self.input[z+kz][y+ky][x+kx]*self.kernel[kz][ky][kx]
+                                o += self.input[kz][y+ky][x+kx]*self.kernel[n][kz][ky][kx]
 
                     row.append(o)
                 plane.append(row)
@@ -70,15 +78,20 @@ image = [[[1,2,3],
          [7,8,9]]]
 
 # initialize the kernel as a 2x2x2 shape and make a forward pass
-conv = Conv([2,2,2])
+conv = Conv([2,2,2],2)
 conv.Pass(image)
 
 # print the outputs
 print("kernel")
-for plane in conv.kernel:
+for neuron in conv.kernel:
     print("")
-    for row in plane:
-        print(*row)
+    print("")
+
+    for plane in neuron:
+        print("")
+
+        for row in plane:
+            print(*row)
 
 print("")
 print("output")
